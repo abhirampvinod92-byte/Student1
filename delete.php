ce37a387a5d5b4e65fcc3ddcf198309427c49aaf
abhirampvinod92-byte <abhirampvinod92@gmail.com>
2025-11-22 20:00:26 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,8 +3,9 @@ include "db.php";
 session_start();
 $id = $_GET['id'];
 
-mysqli_query($conn, "DELETE FROM student WHERE id=$id");
+mysqli_query($conn, "DELETE FROM student1 WHERE id=$id");
 
 header("Location: list.php");
 ?>
+
 
