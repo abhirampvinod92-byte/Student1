9ec4371c4b9fb2668720f1eaf1be89697e0e01eb
abhirampvinod92-byte <abhirampvinod92@gmail.com>
2025-11-22 19:26:46 +0530

    Refactor delete.php to include session handling
    Added session_start() to ensure session management.

--- delete.php
+++ delete.php
@@ -1,10 +1,10 @@
 <?php
 include "db.php";
-if(!isset($_SESSION['admin'])){ header("Location: login.php"); }
-
+session_start();
 $id = $_GET['id'];
 
 mysqli_query($conn, "DELETE FROM student WHERE id=$id");
 
 header("Location: list.php");
 ?>
+
